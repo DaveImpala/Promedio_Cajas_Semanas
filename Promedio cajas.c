093dfa1a36074e879f54377e092164e5de1245e0
Dave <dave_impala@outlook.es>
2016-10-19 17:25:20 -0500

    Conteo Dias
    Arreglando error de promedio, agregando la variable de conteo de dias

--- Promedio cajas.c
+++ Promedio cajas.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 int main()
 {
-    int i=0, cajas[8], totalC=0, dia=1;
+    int i=0, cajas[8], totalC=0, dia=1, ConteoDias=0;
     float promedio;
     char compa;
     do
@@ -11,24 +11,31 @@ int main()
     {
     case 1:
         printf("se trabajo el Lunes\n");
+        ConteoDias++;
         break;
     case 2:
         printf("se trabajo el Martes\n");
+        ConteoDias++;
         break;
     case 3:
         printf("se trabajo el Miercoles\n");
+        ConteoDias++;
         break;
     case 4:
         printf("se trabajo el Jueves\n");
+        ConteoDias++;
         break;
     case 5:
         printf("se trabajo el Viernes\n");
+        ConteoDias++;
         break;
     case 6:
         printf("se trabajo el Sabado\n");
+        ConteoDias++;
         break;
     case 7:
         printf("se trabajo el Domingo\n");
+        ConteoDias++;
         break;
     }
     fflush(stdin);
@@ -47,7 +54,7 @@ int main()
         dia++;
         cajas[dia]=0;
     }while(dia<8);
-    promedio=(float)totalC/7;
+    promedio=(float)totalC/ConteoDias;
     for(dia=1;dia<8;dia++)
     {
         printf("El dia %i se entregaron tantas cajas %i\n",dia, cajas[dia]);
